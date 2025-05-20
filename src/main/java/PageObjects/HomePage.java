56e6bbe270a6dc1b0d7ecdd20734214dd3d6a00e
VinayAdapa <125582210+Vinayadapa777@users.noreply.github.com>
2025-05-20 12:41:51 +0530

    Update HomePage.java

--- src/main/java/PageObjects/HomePage.java
+++ src/main/java/PageObjects/HomePage.java
@@ -31,11 +31,11 @@ public class HomePage extends ActionFunctions {
 	explicitClickable(logoutButton,10);
 	click(logoutButton);
     }
-//dummy
+
     public void clickOnViewProfile() throws InterruptedException{
 	explicitClickable(viewProfile, 10);
 	click(viewProfile);
     }
-    ///test
+
 
 }
